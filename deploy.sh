9e7f7ce0912072c40d1f2f782bb10fc4f23f0bf6
Nikolay Ladygin <nilard@gmail.com>
2024-03-29 23:40:22 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 
 docker compose up -d
 docker compose exec app composer install --no-interaction
+docker compose exec app chmod a+w /app/runtime /app/web/assets
 docker compose exec app php yii migrate --interactive=0
